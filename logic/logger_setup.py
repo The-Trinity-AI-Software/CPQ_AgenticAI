0548d0d6844147d58b5efb3eb052c21660e0b6d6
sumanthg201994 <sumanthg@thetrinitysoftware.com>
2025-04-23 00:02:53 +0530

    Update logger_setup.py

--- logic/logger_setup.py
+++ logic/logger_setup.py
@@ -1,26 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 15 17:31:56 2025
-
-@author: HP
-"""
-
 import logging
-import os
-from datetime import datetime
+import sys
 
-def setup_logger():
-    log_dir = r"/home/CPQ_AgenticAI/MVP/mnt/data/CPQ_Agentic_Workflow/logs"
-    os.makedirs(log_dir, exist_ok=True)
-    
-    log_filename = f"cpq_session_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
-    log_path = os.path.join(log_dir, log_filename)
+def setup_logger(name="CPQApp"):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
 
-    logging.basicConfig(
-        filename=log_path,
-        level=logging.INFO,
-        format="%(asctime)s [%(levelname)s] %(message)s",
-        filemode="w"  # overwrite each time; use "a" to append
-    )
+    if not logger.handlers:
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter(
+            '%(asctime)s - %(levelname)s - %(name)s - %(message)s'
+        )
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
 
-    return logging.getLogger("cpq_logger")
+    return logger
